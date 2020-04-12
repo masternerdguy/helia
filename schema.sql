0dfd1de66359723e111166d6d61cb7754a28b5ad
Nathan T Wiehoff <masternerdguy@yahoo.com>
2020-04-12 01:00:03 -0400

    * added schema file with some stubbed tables

---
+++ schema.sql
@@ -0,0 +1,136 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.2
+-- Dumped by pg_dump version 12.2
+
+-- Started on 2020-04-12 00:58:43
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 202 (class 1259 OID 24583)
+-- Name: universe_regions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.universe_regions (
+    id uuid NOT NULL,
+    regionname character varying(32) NOT NULL
+);
+
+
+ALTER TABLE public.universe_regions OWNER TO postgres;
+
+--
+-- TOC entry 203 (class 1259 OID 24590)
+-- Name: universe_systems; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.universe_systems (
+    id uuid NOT NULL,
+    systemname character varying(32) NOT NULL,
+    regionid uuid NOT NULL
+);
+
+
+ALTER TABLE public.universe_systems OWNER TO postgres;
+
+--
+-- TOC entry 204 (class 1259 OID 24602)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id uuid NOT NULL,
+    username character varying(32) NOT NULL,
+    hashpass character(64) NOT NULL,
+    registered timestamp with time zone NOT NULL,
+    banned bit(1) NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 2694 (class 2606 OID 24589)
+-- Name: universe_regions universe_regions_name_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.universe_regions
+    ADD CONSTRAINT universe_regions_name_uq UNIQUE (regionname);
+
+
+--
+-- TOC entry 2696 (class 2606 OID 24587)
+-- Name: universe_regions universe_regions_pk_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.universe_regions
+    ADD CONSTRAINT universe_regions_pk_uq PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2698 (class 2606 OID 24596)
+-- Name: universe_systems universe_systems_name_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.universe_systems
+    ADD CONSTRAINT universe_systems_name_uq UNIQUE (systemname);
+
+
+--
+-- TOC entry 2700 (class 2606 OID 24594)
+-- Name: universe_systems universe_systems_pk_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.universe_systems
+    ADD CONSTRAINT universe_systems_pk_uq PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2702 (class 2606 OID 24606)
+-- Name: users users_pk_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pk_uq PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2704 (class 2606 OID 24608)
+-- Name: users users_username_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_username_uq UNIQUE (username);
+
+
+--
+-- TOC entry 2705 (class 2606 OID 24597)
+-- Name: universe_systems fk_system_region; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.universe_systems
+    ADD CONSTRAINT fk_system_region FOREIGN KEY (regionid) REFERENCES public.universe_regions(id);
+
+
+-- Completed on 2020-04-12 00:58:43
+
+--
+-- PostgreSQL database dump complete
+--
+
