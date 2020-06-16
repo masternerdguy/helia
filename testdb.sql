a53a218c6cc0504274717e67febf12d33d237f09
Nathan T Wiehoff <masternerdguy@yahoo.com>
2020-06-16 08:50:19 -0400

    * fixed missing schema in testdb.sql

--- testdb.sql
+++ testdb.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.2
 -- Dumped by pg_dump version 12.2
 
--- Started on 2020-06-16 08:46:22
+-- Started on 2020-06-16 08:49:56
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -17,6 +17,108 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+--
+-- TOC entry 2863 (class 1262 OID 16393)
+-- Name: helia; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE helia WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252';
+
+
+ALTER DATABASE helia OWNER TO postgres;
+
+\connect helia
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 205 (class 1259 OID 24609)
+-- Name: sessions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.sessions (
+    id uuid NOT NULL,
+    userid uuid NOT NULL
+);
+
+
+ALTER TABLE public.sessions OWNER TO postgres;
+
+--
+-- TOC entry 206 (class 1259 OID 24616)
+-- Name: ships; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.ships (
+    id uuid NOT NULL,
+    universe_systemid uuid NOT NULL,
+    userid uuid NOT NULL,
+    pos_x double precision DEFAULT 0 NOT NULL,
+    pos_y double precision DEFAULT 0 NOT NULL,
+    created time with time zone DEFAULT now() NOT NULL,
+    shipname character varying(32) NOT NULL
+);
+
+
+ALTER TABLE public.ships OWNER TO postgres;
+
+--
+-- TOC entry 202 (class 1259 OID 24583)
+-- Name: universe_regions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.universe_regions (
+    id uuid NOT NULL,
+    regionname character varying(32) NOT NULL
+);
+
+
+ALTER TABLE public.universe_regions OWNER TO postgres;
+
+--
+-- TOC entry 203 (class 1259 OID 24590)
+-- Name: universe_systems; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.universe_systems (
+    id uuid NOT NULL,
+    systemname character varying(32) NOT NULL,
+    regionid uuid NOT NULL
+);
+
+
+ALTER TABLE public.universe_systems OWNER TO postgres;
+
+--
+-- TOC entry 204 (class 1259 OID 24602)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id uuid NOT NULL,
+    username character varying(32) NOT NULL,
+    hashpass character(64) NOT NULL,
+    registered timestamp with time zone NOT NULL,
+    banned bit(1) NOT NULL,
+    current_shipid uuid
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
 
 --
 -- TOC entry 2856 (class 0 OID 24609)
@@ -74,7 +176,132 @@ COPY public.users (id, username, hashpass, registered, banned, current_shipid) F
 \.
 
 
--- Completed on 2020-06-16 08:46:23
+--
+-- TOC entry 2718 (class 2606 OID 24613)
+-- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT sessions_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2722 (class 2606 OID 24620)
+-- Name: ships ships_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ships
+    ADD CONSTRAINT ships_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2705 (class 2606 OID 24589)
+-- Name: universe_regions universe_regions_name_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.universe_regions
+    ADD CONSTRAINT universe_regions_name_uq UNIQUE (regionname);
+
+
+--
+-- TOC entry 2707 (class 2606 OID 24587)
+-- Name: universe_regions universe_regions_pk_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.universe_regions
+    ADD CONSTRAINT universe_regions_pk_uq PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2709 (class 2606 OID 24596)
+-- Name: universe_systems universe_systems_name_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.universe_systems
+    ADD CONSTRAINT universe_systems_name_uq UNIQUE (systemname);
+
+
+--
+-- TOC entry 2711 (class 2606 OID 24594)
+-- Name: universe_systems universe_systems_pk_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.universe_systems
+    ADD CONSTRAINT universe_systems_pk_uq PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2720 (class 2606 OID 24615)
+-- Name: sessions userid_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sessions
+    ADD CONSTRAINT userid_uq UNIQUE (userid);
+
+
+--
+-- TOC entry 2714 (class 2606 OID 24606)
+-- Name: users users_pk_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pk_uq PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2716 (class 2606 OID 24608)
+-- Name: users users_username_uq; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_username_uq UNIQUE (username);
+
+
+--
+-- TOC entry 2712 (class 1259 OID 24641)
+-- Name: fki_fk_users_ships; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_fk_users_ships ON public.users USING btree (current_shipid);
+
+
+--
+-- TOC entry 2725 (class 2606 OID 24621)
+-- Name: ships fk_ships_systems; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ships
+    ADD CONSTRAINT fk_ships_systems FOREIGN KEY (universe_systemid) REFERENCES public.universe_systems(id);
+
+
+--
+-- TOC entry 2726 (class 2606 OID 24626)
+-- Name: ships fk_ships_users; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ships
+    ADD CONSTRAINT fk_ships_users FOREIGN KEY (userid) REFERENCES public.users(id);
+
+
+--
+-- TOC entry 2723 (class 2606 OID 24597)
+-- Name: universe_systems fk_system_region; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.universe_systems
+    ADD CONSTRAINT fk_system_region FOREIGN KEY (regionid) REFERENCES public.universe_regions(id);
+
+
+--
+-- TOC entry 2724 (class 2606 OID 24636)
+-- Name: users fk_users_ships; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT fk_users_ships FOREIGN KEY (current_shipid) REFERENCES public.ships(id);
+
+
+-- Completed on 2020-06-16 08:49:56
 
 --
 -- PostgreSQL database dump complete
