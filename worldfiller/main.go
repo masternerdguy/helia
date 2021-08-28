47f14397a405c236c81a8ac8d882dc56f079b6db
Nathan Wiehoff <masternerdguy@yahoo.com>
2021-08-28 16:09:15 -0400

    * generating scarcity level for asteroids

--- worldfiller/main.go
+++ worldfiller/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"helia/engine"
 	"helia/universe"
 	"log"
+	"math"
 	"math/rand"
 )
 
@@ -13,9 +15,6 @@ import (
  */
 
 func main() {
-	// initialize RNG
-	rand.Seed(102)
-
 	// load universe from database
 	log.Println("Loading universe from database...")
 	universe, err := engine.LoadUniverse()
@@ -33,7 +32,43 @@ func main() {
 	dropAsteroids(universe)
 }
 
+/* Parameters for asteroid generation */
+const MinAsteroidsPerSystem = 0
+const MaxAsteroidsPerSystem = 100
+
+// Generates a seed integer for use as a system-specific RNG seed for consistent internal generation
+func calculateSystemSeed(s *universe.SolarSystem) int {
+	// use the system's position and uuid timestamp as a seed
+	seed := (int(s.PosX*10000.0)>>int(math.Abs(s.PosY)*10000.0) + s.ID.ClockSequence())
+
+	if s.PosY < 0 {
+		seed *= -1
+	}
+
+	return seed
+}
+
 // Inserts minable asteroids into the universe
-func dropAsteroids(universe *universe.Universe) {
+func dropAsteroids(u *universe.Universe) {
+	for _, r := range u.Regions {
+		for _, s := range r.Systems {
+			// get system internal seed
+			seed := calculateSystemSeed(s)
+
+			// introduce unique offset for this function
+			seed = seed + 50912
+
+			// initialize RNG with seed
+			rand.Seed(int64(seed))
+
+			// get scarcity level
+			scarcity := rand.Float64()
+
+			// print level
+			log.Println(fmt.Sprintf("%v | scarcity: %v", s.SystemName, scarcity))
+		}
+	}
+
+	// use uuid to get the system's random seed
 
 }
