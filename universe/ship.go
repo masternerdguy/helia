21d48fde466ab9d714602bdf3a1dc079c0380d69
Nathan Wiehoff <masternerdguy@yahoo.com>
2021-12-19 00:50:19 -0500

    * added debug out

--- universe/ship.go
+++ universe/ship.go
@@ -3,6 +3,7 @@ package universe
 import (
 	"errors"
 	"fmt"
+	"log"
 	"math"
 	"math/rand"
 	"time"
@@ -4455,6 +4456,8 @@ func (m *FittedSlot) activateAsMissileLauncher() bool {
 }
 
 func (m *FittedSlot) activateAsShieldBooster() bool {
+	log.Println(fmt.Sprintf("%v :: %v", m.shipMountedOn.CharacterName, m.usageExperienceModifier))
+
 	// get shield boost amount
 	shieldBoost, _ := m.ItemMeta.GetFloat64("shield_boost_amount")
 
