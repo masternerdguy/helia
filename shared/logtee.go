4a91aab7a4bb80451d822f44496fd8d32e3244a4
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-01-15 22:31:17 -0500

    * working on tee logging

---
+++ shared/logtee.go
@@ -0,0 +1,57 @@
+package shared
+
+import (
+	"time"
+)
+
+var teeLogChannel chan teeLog
+var teeLogInitialized = false
+var teeLogHandlers []logTeeHandler
+
+type logTeeHandler func(string, time.Time)
+
+type teeLog struct {
+	Message   string
+	EventTime time.Time
+}
+
+// Prepares shared tee logger
+func InitializeTeeLog(fns []logTeeHandler) {
+	if teeLogInitialized {
+		panic("logtee is already initialized!")
+	}
+
+	// initialize channel
+	teeLogChannel = make(chan teeLog)
+	teeLogHandlers = fns
+	teeLogInitialized = true
+
+	// start watcher
+	go func() {
+		for {
+			// wait for log
+			log := <-teeLogChannel
+
+			// pass to logger functions
+			for _, h := range teeLogHandlers {
+				h(log.Message, log.EventTime)
+			}
+		}
+	}()
+}
+
+// Queues a log string to be handled by the initialized logger functions
+func TeeLog(log string) {
+	if !teeLogInitialized {
+		panic("logtee is not initialized!")
+	}
+
+	// get timestamp
+	now := time.Now()
+
+	// send log to channel
+	teeLogChannel <- teeLog{
+		Message:   log,
+		EventTime: now,
+	}
+}
