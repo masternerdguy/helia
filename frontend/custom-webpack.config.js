677b14613c0c8eb91519eb1a77cc60418f2fa89e
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-04-09 02:43:26 -0400

    * implemented build obfuscation

---
+++ frontend/custom-webpack.config.js
@@ -0,0 +1,33 @@
+const WebpackObfuscator = require('webpack-obfuscator');
+
+module.exports = {
+    plugins: [
+        new WebpackObfuscator ({
+            compact: true,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 0.75,
+            deadCodeInjection: true,
+            deadCodeInjectionThreshold: 0.4,
+            debugProtection: false,
+            disableConsoleOutput: false,
+            numbersToExpressions: true,
+            selfDefending: true,
+            simplify: true,
+            sourceMap: false,
+            splitStrings: true,
+            splitStringsChunkLength: 10,
+            stringArray: true,
+            stringArrayCallsTransform: true,
+            stringArrayCallsTransformThreshold: 0.5,
+            stringArrayIndexShift: true,
+            stringArrayRotate: true,
+            stringArrayShuffle: true,
+            stringArrayWrappersCount: 1,
+            stringArrayWrappersChainedCalls: true,
+            stringArrayWrappersParametersMaxCount: 2,
+            stringArrayWrappersType: 'variable',
+            stringArrayThreshold: 1,
+            target: 'browser',
+        }, ['vendor.js', 'scripts.js', 'styles.js', 'runtime.js', 'polyfill.js'])
+    ]
+};
