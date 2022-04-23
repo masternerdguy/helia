3aa92746b4def261a2bde4c56a0cedceb22dce95
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-04-22 21:27:46 -0400

    * working on building docker image

---
+++ bash/build-docker-alpha.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# clean repository
+git stash
+git clean -fxd
+
+# copy alpha config
+cp -v ~/go/src/helia/db-configuration-alpha.json ~/go/src/helia/db-configuration.json
+
+# remove frontend and scripting files
+rm -rfv ~/go/src/helia/frontend
+rm -rfv ~/go/src/helia/bash
+
+# remove sql backups
+rm -rfv ~/go/src/helia/*.sql
+
+# build docker image
+docker build -t heliaalpha . -f Dockerfile.single
+
+# restore repo state
+git clean -fxd
+git reset --hard
+git stash pop
