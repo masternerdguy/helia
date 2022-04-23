9dde38710b205ce840f337fc9e6ddd797b932ee7
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-04-23 03:53:05 -0400

    * changes for self signed ssl during open alpha

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: `http://${window.location.hostname}:8080/api/`,
-  wsUrl: `ws://${window.location.hostname}:8080/ws/`,
+  apiUrl: `https://${window.location.hostname}:8472/api/`,
+  wsUrl: `wss://${window.location.hostname}:8472/ws/`,
 };
 
 /*
