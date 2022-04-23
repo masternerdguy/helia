d01db0e7a72e77839cc80eebb1e9f7ca42eaf68f
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-04-22 23:52:33 -0400

    * including port in check

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
     const queryString = getQueryStringVars();
     let safe = false;
 
-    if (window.location.href.indexOf('localhost') > 0) {
+    if (window.location.href.indexOf('localhost:4200') > 0) {
       safe = true;
     } else {
       for (const k of queryString) {
