7c68e8c7345272c33a21a23938a5955a76806a41
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-04-22 23:51:12 -0400

    * added note to run cli command

--- bash/deploy-docker-alpha.sh
+++ bash/deploy-docker-alpha.sh
@@ -8,3 +8,8 @@ docker tag heliaalpha heliaalpharegistry.azurecr.io/heliaalpha
 
 # push local image to azure
 docker push heliaalpharegistry.azurecr.io/heliaalpha
+
+# inform user to update startup timeout
+echo 'GAME ENGINE DEPLOYMENT COMPLETE :party parrot:'
+echo 'Please run the following on the Azure CLI to avoid a startup timeout: '
+echo '    az webapp config appsettings set --resource-group helia --name helia-alpha-engine --settings WEBSITES_CONTAINER_START_TIME_LIMIT=1800'
