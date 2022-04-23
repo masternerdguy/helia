5a279aaedaedc9be1bd3fdb00cac215e1d9c0b3d
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-04-22 21:57:56 -0400

    * able to push docker image to azure registry

--- bash/deploy-docker-alpha.sh
+++ bash/deploy-docker-alpha.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # login to registry
-docker login heliaalpharegistry.azurecr.io
+docker login heliaalpharegistry.azurecr.io --username 'heliaalpharegistry' --password 'wN/oE7R0+3jxiCKAxfIcdEWv4tqHwUu1'
 
 # tag local image
 docker tag heliaalpha heliaalpharegistry.azurecr.io/heliaalpha
 
 # push local image to azure
-docker push heliaalpha
+docker push heliaalpharegistry.azurecr.io/heliaalpha
