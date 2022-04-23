d98d69f891d39ee76ebfe4493d1bacd97f1df6bf
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-04-22 23:42:36 -0400

    * added a kind of garbage mechanism to lock down the open alpha frontend until the 1st

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,17 +10,56 @@ export class AppComponent {
   title = 'Project Helia';
 
   public ngOnInit() {
-    $(function () {
-      $('.menu-item').on('click', function () {
-        const e = $(this);
-        const href = e.attr('href');
+    // locking during pre-alpha - todo: remove at alpha (this isn't foolproof but it should be sufficient for its purpose)
+    const queryString = getQueryStringVars();
+    let safe = false;
 
-        window.location.href = href;
-      });
+    if (window.location.href.split('?')[0].indexOf('connect') > 0) {
+      safe = true;
+    } else {
+      for (const k of queryString) {
+        if (k == 'safety') {
+          if (queryString[k] == '2058') {
+            safe = true;
+          }
+        }
+      }
+    }
 
-      $('#header-branding').on('click', function () {
-        window.location.href = '/';
+    if (!safe) {
+      // locked out
+      $('body').remove();
+      alert('Not yet in open alpha - coming soon :)');
+    } else {
+      // normal initialization - keep after removing lock
+      $(function () {
+        $('.menu-item').on('click', function () {
+          const e = $(this);
+          const href = e.attr('href');
+
+          window.location.href = href;
+        });
+
+        $('#header-branding').on('click', function () {
+          window.location.href = '/';
+        });
       });
-    });
+    }
   }
+}
+
+function getQueryStringVars(): string[] {
+  const pairs: string[] = [];
+  const keys = window.location.href
+    .slice(window.location.href.indexOf('?') + 1)
+    .split('&');
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i].split('=');
+
+    pairs.push(key[0]);
+    pairs[key[0]] = key[1];
+  }
+
+  return pairs;
 }
