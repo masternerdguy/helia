b6b9090f0f906185d49156fee4587a659d49ad25
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-04-24 22:46:40 -0400

    * cleanup up package.json

--- bash/self-sign-cert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# generate self signed cert based on config
-openssl req -config ~/go/src/helia/ssl-cert.conf -new -x509 -newkey rsa:2048 -nodes -keyout ~/go/src/helia/ssl.key.pem -days 365 -out ~/go/src/helia/ssl.cert.pem
-
-# all done!
-echo "Eyup..."
