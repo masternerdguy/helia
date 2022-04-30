cf3cdfb8f03710a5495b654ba61b9d4c694a56bf
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-04-30 15:57:12 -0400

    * added frontend lore for originals, kingdom, and accord

--- frontend/src/app/lore/lore.component.ts
+++ frontend/src/app/lore/lore.component.ts
@@ -3,6 +3,9 @@ import { IFactionLoreData } from './factions/iFactionLoreData';
 import { WanderersLoreData } from './factions/wanderers';
 import * as $ from 'jquery';
 import { InterstarLoreData } from './factions/interstar';
+import { OriginalsLoreData } from './factions/originals';
+import { KingdomLoreData } from './factions/kingdom';
+import { AccordLoreData } from './factions/accord';
 
 @Component({
   selector: 'app-lore',
@@ -11,6 +14,9 @@ import { InterstarLoreData } from './factions/interstar';
 })
 export class LoreComponent implements OnInit {
   factionLoreData: IFactionLoreData[] = [
+    new OriginalsLoreData(),
+    new KingdomLoreData(),
+    new AccordLoreData(),
     new InterstarLoreData(),
     new WanderersLoreData(),
   ];
