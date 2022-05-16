9d92a02210a72c8013e6bb936ffd2fb8fef7d67c
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-05-16 02:18:45 -0400

    * fixed restore script

--- bash/backup-db-dev.sh
+++ bash/backup-db-dev.sh
@@ -2,4 +2,7 @@
 
 export PGPASSWORD='fdb01db4749748cfbdb21e4766570561!'
 pg_dump -h 127.0.0.1 -d helia -U heliaagent -f ../devschema.sql -s
-pg_dump -h 127.0.0.1 -d helia -U heliaagent --column-inserts --data-only -f ../devdb.sql
+pg_dump -h 127.0.0.1 -d helia -U heliaagent -f ../devdb.sql
+
+
+# --column-inserts --data-only
