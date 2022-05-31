761a94890dfd5a0e9938fd559569954cfdaf20fb
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-05-30 20:52:18 -0400

    * rebaselined flyway

--- flyway/sql/V3.6__DevHax.sql
+++
@@ -1,6 +0,0 @@
--- add flag for users to be marked as developers
-ALTER TABLE IF EXISTS public.users
-    ADD COLUMN isdev boolean NOT NULL DEFAULT 'f';
-
--- make me a dev
-update users set isdev = true where id = 'ca23e222-bf73-4f40-968f-715c033f12b2'
