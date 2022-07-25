b4e840b472745d8c587ccb83e48d4e0a3f15f3bd
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-07-24 22:36:21 -0400

    * added item family entries

--- flyway/sql/V1.1__EwarModules.sql
+++ flyway/sql/V1.1__EwarModules.sql
@@ -1,1 +1,6 @@
--- module families
+-- item families
+INSERT INTO public.itemfamilies (id, friendlyname, meta) VALUES ('ewar_cycle', 'Cycle Disruptor', '{}');
+INSERT INTO public.itemfamilies (id, friendlyname, meta) VALUES ('ewar_fcj', 'Fire Control Jammer', '{}');
+INSERT INTO public.itemfamilies (id, friendlyname, meta) VALUES ('ewar_r_mask', 'Regeneration Mask', '{}');
+INSERT INTO public.itemfamilies (id, friendlyname, meta) VALUES ('ewar_d_mask', 'Dissipation Mask', '{}');
+
