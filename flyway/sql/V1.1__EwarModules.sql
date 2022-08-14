e065710f57df1eeceae262bd7ae9f290419a8726
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-08-14 18:35:25 -0400

    * forgot process i/o

--- flyway/sql/V1.1__EwarModules.sql
+++ flyway/sql/V1.1__EwarModules.sql
@@ -33,7 +33,19 @@ INSERT INTO public.processinputs (id, itemtypeid, quantity, meta, processid) VAL
 INSERT INTO public.processinputs (id, itemtypeid, quantity, meta, processid) VALUES ('294b3eb4-787f-47b4-a252-8f64da26d617', '39b8eedf-ef80-4c29-a4bf-99abc4d84fa6', 1, '{}', '3768279c-849f-4c96-a447-9d4bf4865271');
 INSERT INTO public.processinputs (id, itemtypeid, quantity, meta, processid) VALUES ('e569ffef-0cbf-4360-a8e8-2929b7fef10e', 'dd522f03-2f52-4e82-b2f8-d7e0029cb82f', 9, '{}', '3768279c-849f-4c96-a447-9d4bf4865271');
 
+INSERT INTO public.processinputs (id, itemtypeid, quantity, meta, processid) VALUES ('cb59948e-8fee-4edf-a18a-8100c2edad64', '32acc0b3-5941-4536-ac6a-3721b7fc0c55', 4, '{}', '2fc8eca7-d513-45e4-808c-8d6cb21d8553');
+INSERT INTO public.processinputs (id, itemtypeid, quantity, meta, processid) VALUES ('e0b8236f-c9a4-4490-95ca-5635ac837abb', '614434d0-eae6-4e41-bd02-9cac795b22c5', 16, '{}', '8863ada9-26b8-4fc4-8cc7-b2b904bf6272');
+INSERT INTO public.processinputs (id, itemtypeid, quantity, meta, processid) VALUES ('e2c4e539-2847-46ee-abeb-59422b28da33', 'dd522f03-2f52-4e82-b2f8-d7e0029cb82f', 28, '{}', '6db05608-bd73-4bc7-a2b0-1fb4c7531307');
+INSERT INTO public.processinputs (id, itemtypeid, quantity, meta, processid) VALUES ('d306730d-8517-4b2e-9460-4aada080db2e', '24800206-2c58-45b0-8238-81974d0ebb3b', 208, '{}', '6db05608-bd73-4bc7-a2b0-1fb4c7531307');
+INSERT INTO public.processinputs (id, itemtypeid, quantity, meta, processid) VALUES ('8a1fca40-1203-4e58-9ac9-8e37bb69a36e', '11688112-f3d4-4d30-864a-684a8b96ea23', 6, '{}', '6db05608-bd73-4bc7-a2b0-1fb4c7531307');
+INSERT INTO public.processinputs (id, itemtypeid, quantity, meta, processid) VALUES ('ccc2a14f-d999-4a5b-8337-71672164078f', '56617d30-6c30-425c-84bf-2484ae8c1156', 6, '{}', '6db05608-bd73-4bc7-a2b0-1fb4c7531307');
+INSERT INTO public.processinputs (id, itemtypeid, quantity, meta, processid) VALUES ('25099548-06ec-47a1-9678-da75360f7594', '2ce48bef-f06b-4550-b20c-0e64864db051', 5, '{}', '6db05608-bd73-4bc7-a2b0-1fb4c7531307');
+
 -- process outputs
 INSERT INTO public.processoutputs (id, itemtypeid, quantity, meta, processid) VALUES ('2e07ac97-6798-40ed-86c9-340423e27f71', '34b6e8ab-cba8-4fe9-81b6-429ef997a01f', 8, '{}', '9eefa1d3-1203-4fc8-b09a-3e4c967965a5');
 INSERT INTO public.processoutputs (id, itemtypeid, quantity, meta, processid) VALUES ('1583610f-6e33-433c-a9b3-0a115fe29e7c', '5c1049c4-f631-4066-9f2a-b0798b2c4399', 368, '{}', '3768279c-849f-4c96-a447-9d4bf4865271');
 INSERT INTO public.processoutputs (id, itemtypeid, quantity, meta, processid) VALUES ('2b6a90cb-331d-4c82-a489-8ce82f25971d', '5c40d8ec-a269-4ae5-912f-1678b6c202a5', 10, '{}', '3768279c-849f-4c96-a447-9d4bf4865271');
+
+INSERT INTO public.processoutputs (id, itemtypeid, quantity, meta, processid) VALUES ('59c117a3-6e55-41ca-9aa8-e405838513db', '32acc0b3-5941-4536-ac6a-3721b7fc0c55', 5, '{}', 'e747ec63-3856-4fd1-8d4b-9eb8779f02e9');
+INSERT INTO public.processoutputs (id, itemtypeid, quantity, meta, processid) VALUES ('783b6114-4c26-40bd-b5dd-a7a0fec60bce', '5c1049c4-f631-4066-9f2a-b0798b2c4399', 208, '{}', '6db05608-bd73-4bc7-a2b0-1fb4c7531307');
+INSERT INTO public.processoutputs (id, itemtypeid, quantity, meta, processid) VALUES ('c98a5192-a152-448f-9be4-c1b3acb4d1c2', '614434d0-eae6-4e41-bd02-9cac795b22c5', 25, '{}', '6db05608-bd73-4bc7-a2b0-1fb4c7531307');
