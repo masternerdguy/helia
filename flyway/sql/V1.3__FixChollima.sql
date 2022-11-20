02a884c58b5ac04f16f7118ddfcd2ad481334a1d
Nathan Wiehoff <masternerdguy@yahoo.com>
2022-11-20 17:37:53 -0500

    * fixed chollima missile slot

---
+++ flyway/sql/V1.3__FixChollima.sql
@@ -0,0 +1,162 @@
+update shiptemplates set slotlayout =
+'{
+  "a_slots": [
+    {
+      "hp_pos": [
+        80,
+        0
+      ],
+      "volume": 450,
+      "mod_family": "missile"
+    },
+    {
+      "hp_pos": [
+        60,
+        0
+      ],
+      "volume": 450,
+      "mod_family": "missile"
+    },
+    {
+      "hp_pos": [
+        40,
+        0
+      ],
+      "volume": 450,
+      "mod_family": "missile"
+    },
+    {
+      "hp_pos": [
+        20,
+        0
+      ],
+      "volume": 450,
+      "mod_family": "missile"
+    },
+    {
+      "hp_pos": [
+        -20,
+        0
+      ],
+      "volume": 310,
+      "mod_family": "gun"
+    },
+    {
+      "hp_pos": [
+        -40,
+        0
+      ],
+      "volume": 310,
+      "mod_family": "gun"
+    },
+    {
+      "hp_pos": [
+        -60,
+        0
+      ],
+      "volume": 245,
+      "mod_family": "utility"
+    },
+    {
+      "hp_pos": [
+        -80,
+        0
+      ],
+      "volume": 245,
+      "mod_family": "utility"
+    }
+  ],
+  "b_slots": [
+    {
+      "hp_pos": [
+        0,
+        0
+      ],
+      "volume": 400,
+      "mod_family": "any"
+    },
+    {
+      "hp_pos": [
+        0,
+        0
+      ],
+      "volume": 400,
+      "mod_family": "any"
+    },
+    {
+      "hp_pos": [
+        0,
+        0
+      ],
+      "volume": 400,
+      "mod_family": "any"
+    },
+    {
+      "hp_pos": [
+        0,
+        0
+      ],
+      "volume": 400,
+      "mod_family": "any"
+    },
+    {
+      "hp_pos": [
+        0,
+        0
+      ],
+      "volume": 400,
+      "mod_family": "any"
+    },
+    {
+      "hp_pos": [
+        0,
+        0
+      ],
+      "volume": 400,
+      "mod_family": "any"
+    }
+  ],
+  "c_slots": [
+    {
+      "hp_pos": [
+        0,
+        0
+      ],
+      "volume": 275,
+      "mod_family": "any"
+    },
+    {
+      "hp_pos": [
+        0,
+        0
+      ],
+      "volume": 275,
+      "mod_family": "any"
+    },
+    {
+      "hp_pos": [
+        0,
+        0
+      ],
+      "volume": 275,
+      "mod_family": "any"
+    },
+    {
+      "hp_pos": [
+        0,
+        0
+      ],
+      "volume": 275,
+      "mod_family": "any"
+    },
+    {
+      "hp_pos": [
+        0,
+        0
+      ],
+      "volume": 275,
+      "mod_family": "any"
+    }
+  ]
+}'
+where id = 'e2a67722-10d1-42fd-9a7c-057677ef6e79';
