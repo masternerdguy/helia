92fd17e4c1bc8ecd83e0997775a475eb9c9ed427
Nathan Wiehoff <masternerdguy@yahoo.com>
2023-06-24 20:15:34 -0400

    * cleaning up scripts

--- bash/backup-db.sh
+++ bash/backup-db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export PGPASSWORD="$POSTGRES_PASSWORD"
+
 pg_dump -h 127.0.0.1 -d $POSTGRES_DB -U $POSTGRES_USER  -f /src/helia/devschema.sql -s
 pg_dump -h 127.0.0.1 -d $POSTGRES_DB -U $POSTGRES_USER  -f /src/helia/devdb.sql #--column-inserts --data-only
 
