9707a94855433733ecf190744b612650eeecc926
Nathan Wiehoff <masternerdguy@yahoo.com>
2024-07-13 11:06:22 -0400

    * starting gas collectors

---
+++ flyway/sql/V1.7__GasModules.sql
@@ -0,0 +1,5 @@
+-- item family
+INSERT INTO public.itemfamilies (id, friendlyname, meta) VALUES ('utility_wisper', 'Wisp Collector', '{}');
+
+-- items
+INSERT INTO public.itemtypes (id, family, name, meta) VALUES ('572b92e7-149d-4129-a9a6-91b2e92999a8', 'utility_wisper', 'Basic Wisp Collector', '{"hp": 8, "rack": "a", "volume": 13, "cooldown": 3.6, "intake_area": 3.1, "can_mine_gas": true, "can_mine_ice": false, "can_mine_ore": false, "needs_target": false, "activation_heat": 6, "industrialmarket": {"maxprice": 14577, "minprice": 11324, "silosize": 350}, "activation_energy": 35}');
