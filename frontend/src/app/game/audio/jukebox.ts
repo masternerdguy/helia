1e85d6545f0736b29fb801ce91f742aba0e2a48b
Nathan Wiehoff <masternerdguy@yahoo.com>
2024-09-07 14:48:36 -0400

    * ran pretty

--- frontend/src/app/game/audio/jukebox.ts
+++ frontend/src/app/game/audio/jukebox.ts
@@ -1,4 +1,4 @@
-import { SoundtrackDataRepository } from "../data/soundtrackData";
+import { SoundtrackDataRepository } from '../data/soundtrackData';
 import { Howl } from 'howler';
 
 let playing: boolean = false;
@@ -6,35 +6,35 @@ let volume: number = 0.75;
 let sound: Howl = undefined;
 
 export class Jukebox {
-    begin() {
-        // begin playback
-        playTrack(nextTrack());
+  begin() {
+    // begin playback
+    playTrack(nextTrack());
 
-        // mark as started
-        playing = true;
-    }
+    // mark as started
+    playing = true;
+  }
 }
 
 function nextTrack(): string {
-    return new SoundtrackDataRepository().randomTrack();
+  return new SoundtrackDataRepository().randomTrack();
 }
 
 function playTrack(t: string) {
-    // load next track and hook following
-    sound = new Howl({
-        src: ['/assets/music/' + t],
-        html5: true,
-        loop: false,
-        volume: volume,
-        onend: function () {
-            // to prevent overflows
-            setTimeout(() => {
-                // play next random track
-                playTrack(nextTrack());
-            }, 100);
-        }
-    });
+  // load next track and hook following
+  sound = new Howl({
+    src: ['/assets/music/' + t],
+    html5: true,
+    loop: false,
+    volume: volume,
+    onend: function () {
+      // to prevent overflows
+      setTimeout(() => {
+        // play next random track
+        playTrack(nextTrack());
+      }, 100);
+    },
+  });
 
-    // start playing
-    sound.play();
+  // start playing
+  sound.play();
 }
